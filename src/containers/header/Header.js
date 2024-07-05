2c262fcedc26191d0383d078570c77ccc4f0a6b1
beatitude200 <beatitude102@gmail.com>
2024-07-05 21:44:42 +0100

    update

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -3,7 +3,16 @@ import "./header.css"
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let’s Build Something
+amazing with GPT-3
+OpenAI
+</h1>
+
+      </div>
+   
+      </div>
   )
 }
 
