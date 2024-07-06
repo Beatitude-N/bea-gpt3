7cf7bcbee3510731e027cccd06de34cb4da63f83
beatitude200 <beatitude102@gmail.com>
2024-07-06 15:58:34 +0100

    a

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./header.css"
 import people from "../../assets/people.png"
+import ai from "../../assets/ai.png"
 function Header() {
   return (
     <div className='gpt3__header section__padding' id='home'>
@@ -15,7 +16,11 @@ function Header() {
         <button type='button'>Get Started</button>
       </div>
       <div className='gpt3__header-content__people'>
-        <img src={people}/>
+        <img src={people} alt='people'/>
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+      <div className='gpt3__header-image'>
+        <img src={ai} alt='ai' />
       </div>
 
 
