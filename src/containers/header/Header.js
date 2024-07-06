f1b08494ee5258fb625e1acd8fb4b0c308bcbad9
beatitude200 <beatitude102@gmail.com>
2024-07-06 15:39:53 +0100

    update

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -6,11 +6,13 @@ function Header() {
     <div className='gpt3__header section__padding' id='home'>
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>Let’s Build Something
-amazing with GPT-3
-OpenAI
-</h1>
-
-
+        amazing with GPT-3
+        OpenAI
+      </h1>
+      <div className='gpt3__header-content__input'>
+        <input type='email' placeholder='Your Email Address'/>
+        <button type='button'>Get Started</button>
+      </div>
       </div>
    
       </div>
